5dd20199b25a95bd2cbd92c803940cbf35d675ea
alazeprt <nono135246@126.com>
2023-03-16 19:10:36 +0800

    Update

---
+++ src/main/java/org/alazeprt/SolarPanelMod.java
@@ -0,0 +1,31 @@
+package org.alazeprt;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.alazeprt.block.solar_panel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SolarPanelMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("solar");
+
+	public static final solar_panel SOLAR_PANEL = Registry.register(Registry.BLOCK,
+			new Identifier("solar", "solar_panel"),
+			new solar_panel(FabricBlockSettings.of(Material.STONE).strength(5.0f, 6.0f).requiresTool().mapColor(MapColor.BLUE)));
+
+
+	@Override
+	public void onInitialize() {
+		Registry.register(Registry.ITEM, new Identifier("solar", "solar_panel"), new BlockItem(SOLAR_PANEL, new FabricItemSettings()));
+		LOGGER.info("Solar Panel Mod v1.0-alpha Loaded");
+	}
+}
---
+++ src/main/java/org/alazeprt/block/solar_panel.java
@@ -0,0 +1,26 @@
+package org.alazeprt.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+import java.util.List;
+
+public class solar_panel extends Block {
+    public solar_panel(Settings settings){
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.2f, 1f);
+    }
+
+}
