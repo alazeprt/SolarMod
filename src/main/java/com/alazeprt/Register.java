bedc7f45cf5877133eb66fffb500405fbff64b10
alazeprt <nono135246@126.com>
2023-03-19 21:04:22 +0800

    Update Silicon Ore

--- src/main/java/com/alazeprt/Register.java
+++ src/main/java/com/alazeprt/Register.java
@@ -1,14 +1,29 @@
 package com.alazeprt;
 
+import com.alazeprt.block.silicon_ore;
 import com.alazeprt.block.solar_panel;
 import com.alazeprt.block.vertical_solar_panel;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
+
+import java.util.Arrays;
 
 public class Register {
     public static void register(){
@@ -18,11 +33,32 @@ public class Register {
         final vertical_solar_panel VERTICAL_SOLAR_PANEL = Registry.register(Registry.BLOCK,
                 new Identifier("solar", "vertical_solar_panel"),
                 new vertical_solar_panel(FabricBlockSettings.of(Material.METAL).strength(5.0f, 6.0f).requiresTool().mapColor(MapColor.BLUE)));
+        final silicon_ore SILICON_ORE = Registry.register(Registry.BLOCK,
+                new Identifier("solar", "silicon_ore"),
+                new silicon_ore(FabricBlockSettings.of(Material.STONE).strength(3.0f, 3.0f).requiresTool()));
         Registry.register(Registry.ITEM,
                 new Identifier("solar", "solar_panel"),
                 new BlockItem(SOLAR_PANEL, new FabricItemSettings()));
         Registry.register(Registry.ITEM,
                 new Identifier("solar", "vertical_solar_panel"),
                 new BlockItem(VERTICAL_SOLAR_PANEL, new FabricItemSettings()));
+        Registry.register(Registry.ITEM,
+                new Identifier("solar", "silicon_ore"),
+                new BlockItem(SILICON_ORE, new FabricItemSettings()));
+        ConfiguredFeature<?, ?> OVERWORLD_WOOL_ORE_CONFIGURED_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE,
+                new Identifier("solar", "silicon_ore"),
+                new ConfiguredFeature(Feature.ORE,
+                        new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, SILICON_ORE.getDefaultState(), 6)));
+        PlacedFeature OVERWORLD_WOOL_ORE_PLACED_FEATURE = Registry.register(BuiltinRegistries.PLACED_FEATURE,
+                new Identifier("solar", "silicon_ore"),
+                new PlacedFeature(RegistryEntry.of(OVERWORLD_WOOL_ORE_CONFIGURED_FEATURE),
+                Arrays.asList(
+                        CountPlacementModifier.of(16),
+                        SquarePlacementModifier.of(),
+                        HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))
+                )));
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, 
+                RegistryKey.of(Registry.PLACED_FEATURE_KEY,
+                        new Identifier("solar", "silicon_ore")));
     }
 }
---
+++ src/main/java/com/alazeprt/block/silicon_ore.java
@@ -0,0 +1,9 @@
+package com.alazeprt.block;
+
+import net.minecraft.block.Block;
+
+public class silicon_ore extends Block {
+    public silicon_ore(Settings settings){
+        super(settings);
+    }
+}
