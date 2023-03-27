2511d89ae3271b2581a07f938dfcec32646c6129
alazeprt <nono135246@126.com>
2023-03-27 13:18:37 +0800

    1.2.1-develop

--- src/main/java/com/alazeprt/block/solar_panel.java
+++ src/main/java/com/alazeprt/block/solar_panel.java
@@ -55,6 +55,8 @@ public class solar_panel extends BlockWithEntity implements BlockEntityProvider,
     @Override
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         if (state.get(WATERLOGGED)) {
+            solar_panel_entity.light = 0;
+            getDefaultState().with(LIGHT, 0);
             world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
         }
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
